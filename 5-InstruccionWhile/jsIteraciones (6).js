8bc441866cd67ba3db8cc6cf9e21fb32b65c21c7
alumno <alumno@utnfra.com.ar>
2020-02-19 22:04:39 -0300

    Clase 5.2

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -3,10 +3,22 @@ function mostrar()
 
 	var contador=0;
 	var acumulador=0;
+	var dato;
 
+	//dato = prompt("Ingrese un valor");
 
+	while(contador<5){
+	contador++;
+	dato = prompt("Ingrese un numero");
+	dato = parseInt(dato);
+		while(isNaN(dato)){
+		dato = prompt("Ingrese un numero valido");
+		dato = parseInt(dato);
+		}
+	acumulador=acumulador+dato;
+	}
 
-document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/5;
+	document.getElementById('suma').value=acumulador;
+	document.getElementById('promedio').value=acumulador/5;
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -3,8 +3,19 @@ function mostrar()
 
 	var contador=0;
 	var acumulador=0;
-	var respuesta='si';
+	var respuesta=true;
 
+	while(respuesta){
+		contador++;
+		numero = prompt("Ingrese un valor");
+		numero = parseInt(numero);
+		while(isNaN(numero)){
+			numero = prompt("Ingrese un valor valido");
+			numero = parseInt(numero);
+		}
+		acumulador = acumulador + numero;
+		respuesta = confirm("Desea continuar?");
+	}
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/contador;
