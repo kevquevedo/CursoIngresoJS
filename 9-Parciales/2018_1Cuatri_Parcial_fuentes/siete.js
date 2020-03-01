9e0dfd70aaf098df15cdc0b6c6bac4ef31a1fbc7
kevquevedo <quevedo.kevin@hotmail.com>
2020-03-01 00:02:01 -0300

    Practica Casa

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -2,22 +2,39 @@ function mostrar()
 {
     var nota;
     var sexo;
-    var contadorAlumno=0;
-
-    while(contadorAlumno<5){
-
-        contadorAlumno++;
-        do {
+    var contador=0;
+    var contadorVarones=0;
+    var acumuladorNota=0;
+    var primeraVez = true;
+    var notaMinima;
+    var sexoMinimo;
+    
+    do{
+        do{
             nota = prompt("Por favor, ingrese una nota");
             nota = parseInt(nota);
-        } while(isNaN(contadorAlumno) || nota>0 || nota<10)
-            //nota = prompt("Por favor, ingrese una nota valida");
-            //nota = parseInt(nota);
-         do {
-            sexo = prompt("Por favor, ingrese un sexo (F ó M)");
-        } while(sexo.toLowerCase!="f" && sexo.toLowerCase!="m"){
-            alert("El sexo ingresado es inválido");
-            //sexo = prompt("Por favor, ingrese un sexo (F ó M)");
+        } while ( isNaN(nota) || nota < 0 || nota > 10);
+        do{
+            sexo = prompt("Por favor, ingrese el sexo");
+        } while ( sexo != "f" && sexo != "m");
+
+        if (primeraVez){
+            primeraVez = false;
+            notaMinima = nota;
+            sexoMinimo = sexo;
+        } else if (nota < notaMinima){
+            notaMinima = nota;
+            sexoMinimo = sexo;
         }
-    }
+        if ( sexo == "m" && nota >= 6){
+            contadorVarones++;
+        }
+        contador++;
+        acumuladorNota+=nota;
+    } while (contador<5);
+   
+    promedioNota = acumuladorNota / contador;
+    alert("El promedio de las notas es: "+promedioNota);
+    alert("La nota mas baja es "+notaMinima+" , y su sexo es "+sexoMinimo);
+    alert("La cantidad de varones que su nota es mayor o igual a 6 es "+contadorVarones);
 }
